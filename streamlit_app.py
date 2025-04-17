cc15f85258a59293c6b5602fb198b25d0c0f2b9c
sborad26 <shubhborad@gmail.com>
2025-04-16 20:20:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 # Import python packages
 import streamlit as st
+import requests
+
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -32,6 +34,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
     st.write(ingredients_string)
 
@@ -43,7 +48,3 @@ if ingredients_string:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
