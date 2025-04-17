29692908019801601f9ab0176315a7db7ae2cd2e
sborad26 <shubhborad@gmail.com>
2025-04-17 11:02:30 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.stop()
 
 ingredients_list= st.multiselect('Choose up to 5 ingredients:',my_dataframe, max_selections= 5)
 
